90f9012da715cd691ea77b6c7818f319ee3100cf
Preetisam <preetikamilla@gmail.com>
2023-06-17 19:42:43 +0530

    1

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB_NAME: "crmbac_db",
-  DB_URL: "mongodb://127.0.0.1:27017/crmbac_db",
+  DB_URL: "mongodb+srv://preetikamilla:H8pIfXymOLgDeJoy@backend-crm.wtwmgu8.mongodb.net/",
 };
